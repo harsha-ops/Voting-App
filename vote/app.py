484f5ae7a9387d67c6abc36bba7f7fec34da33a8
harsha <harsha.venkata220@gmail.com>
2024-12-06 16:34:59 +0000

    change in vote deploy yml file in k8s

--- vote/app.py
+++ vote/app.py
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        redis_host = os.getenv('REDIS_HOST', 'redis')  # Default to 'redis' if not set
+        redis_host = os.getenv('REDIS_HOST', 'redis-svc')  # Default to 'redis' if not set
         redis_port = int(os.getenv('REDIS_PORT', 6379))  # Default to 6379 if not set
         g.redis = Redis(host=redis_host, port=redis_port, db=0, socket_timeout=5)
     return g.redis
