669170f163cc203a64e2e528bf831af1f3560feb
harsha <harsha.venkata220@gmail.com>
2024-12-06 16:20:53 +0000

    add redis function in vote app file

--- vote/app.py
+++ vote/app.py
@@ -18,7 +18,9 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        redis_host = os.getenv('REDIS_HOST', 'redis')  # Default to 'redis' if not set
+        redis_port = int(os.getenv('REDIS_PORT', 6379))  # Default to 6379 if not set
+        g.redis = Redis(host=redis_host, port=redis_port, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
